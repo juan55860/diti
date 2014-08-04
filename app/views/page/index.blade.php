9ee556855753fa06bb18c2d253cf5e2a5f0e3090
juan55860 <juan55860@gmail.com>
2014-08-04 12:22:15 -0500

    Laravel app
    use way/generators for Migrations
    use fzaninotto/faker for Seeders
    
    Model database in Migration

---
+++ app/views/page/index.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<title>@yield('title', 'Diti')</title>	
+
+</head>
+
+<body>
+
+	<base href="/">
+
+	<header>
+
+	</header>
+
+	<div class="container">
+		
+		<div class="center">
+			<div class="pull-left">
+				<h1>Primera aplicación en Laravel</h1>
+				<br>
+				<h3>Página de inicio</h3>
+				<br>
+				<h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum p.</h4>
+					<br>
+					<br>
+
+				</div>
+
+			</div>
+		</div>
+		<br><br><br><br>
+		<footer>
+			<div class="container">
+				<h5>Terminos y Condiciones</h5>
+			</div>
+		</footer>
+
+
+	</body>
+	</html>
