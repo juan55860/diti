db0336c8d9d90d83925de372f66ca177121c22f2
juan55860 <juan55860@gmail.com>
2014-08-04 14:38:32 -0500

    Boostrap
    Integration bootstrap template

--- app/routes.php
+++ app/routes.php
@@ -1,18 +1,9 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
 
-Route::get('/', function()
-{
+
+//Route::get('/', function()
+//{
 	//return View::make('hello'); // asi venia de laravel
-	return View::make('page.index');
-});
+Route::get('/', ['as' =>'home','uses' =>'HomeController@index']);
+//});
